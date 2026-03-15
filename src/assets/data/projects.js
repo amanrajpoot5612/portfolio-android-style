5b4eeca5f4fdaf11266a38e672447ad58cd47146
Aman Rajpoot <amanrajpoot5612@gmail.com>
2026-03-15 12:32:11 +0530

    feat: add new app icons and project data, remove outdated icons

---
+++ src/assets/data/projects.js
@@ -0,0 +1,32 @@
+import Bugsy from "../projects/bugsy.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Full-Stack Video Streaming & Sharing Platform",
+    image: Bugsy,
+    tech: ["Node.js", "Express", "MongoDB"],
+    description:
+      "Authentication system with JWT, session ID and watch history.",
+    github: "https://github.com/...",
+    live: "https://...",
+  },
+  {
+    id: 2,
+    title: "Android Style Portfolio Website",
+    image: Bugsy,
+    tech: ["React", "Tailwind"],
+    description: "Android inspired interactive portfolio interface.",
+    github: "",
+    live: "",
+  },
+  {
+    id: 3,
+    title: "Portfolio OS",
+    image: Bugsy,
+    tech: ["React", "Tailwind"],
+    description: "Android inspired interactive portfolio interface.",
+    github: "",
+    live: "",
+  },
+];
